8f68566675005edd1bb1acb7fad49f4b0bcf971c
yintingwen <yintingwen@foxmail.com>
2021-02-25 17:58:51 +0800

    修改声明文件

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,28 @@
-declare module '@ytw/vue2-clipboard' {
-  import Vue, { PluginFunction } from 'vue'
-  module "vue/types/vue" {
-    interface Vue {
-      $copyText(text: string, options: { container?: string | HTMLElement, callHooks: boolean }): Promise<any>
-    }
-  }
 
-  const Vue2Clipboard: { 
-    install: PluginFunction<never> 
-  }
+import Vue, { PluginFunction } from 'vue'
 
-  export const mergeOptions: { 
-    success: () => any,
-    error: () => any,
-    callHooks: boolean 
-  }
-
-  export const copyText: (text: string, options: {
-    container?: string | HTMLElement, 
-    callHooks: boolean
-  }) => any
-
-  export default Vue2Clipboard
+interface methodOptions {
+  container? :string | HTMLElement,
+  callHooks?: boolean
 }
+declare module "vue/types/vue" {
+  interface Vue {
+    $copyText(text: string, options: methodOptions): Promise<any>
+  }
+}
+
+export declare interface Vue2Clipboard {
+  install: PluginFunction<never>
+}
+
+export declare interface mergeOptions {
+  success (): any
+  error (): any
+  callHooks?: boolean
+}
+
+export declare interface copyText {
+  (text: string, options: methodOptions): Promise<any>
+}
+
+export default Vue2Clipboard
