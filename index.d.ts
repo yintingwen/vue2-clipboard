8769b2ba218d2d7898c5eed4d66c22241e42f44b
殷庭文 <yintingwen@qifangyun.com>
2021-07-19 14:29:35 +0800

    修改.d.type

---
+++ index.d.ts
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+
+interface CopyText {
+  (text: string, options: { container?: string | HTMLElement, callHooks?: boolean }): Promise<any>
+}
+
+interface GlobalOptions {
+  success?(): any
+  error?(): any
+  callHooks?: boolean
+}
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $copyText: CopyText
+  }
+}
+
+declare class Vue2Clipboard {
+  static install(vue: typeof Vue, options: GlobalOptions): void
+}
+
+export const mergeOptions: (options: GlobalOptions) => void
+export const copyText: CopyText
+export default Vue2Clipboard
